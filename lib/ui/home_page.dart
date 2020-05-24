939f4ca4832f760d564e1f4705c0092cb247aadc
Divino Rodrigues <divino.sr@hotmail.com>
2020-05-23 23:44:23 -0300

    Requesting the data

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +8,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=BYMk1WgF8se6ltuD0CWsHUNccPCPpojb&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=BYMk1WgF8se6ltuD0CWsHUNccPCPpojb&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
